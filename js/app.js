f7279fe24c0cf0b97e9afd80d6eacd71c31b4b90
Rodolf Sabalburo <rodolf.sabalburo@gmail.com>
2015-10-02 19:19:22 +0400

    hot-or-cold-starter: add support for new game button
    Wrapped everything inside newGame() function. Changed gameNumber to
    winningNumber which is now a global. Clicking a.new button simply
    stores new generateRandomNumber() function inside winningNumber.

--- js/app.js
+++ js/app.js
@@ -11,58 +11,71 @@ $(document).ready(function(){
     $(".overlay").fadeOut(1000);
   });
 
-  function generateRandomNumber() {
-    return parseInt(Math.random() * 100);
-  }
+  function newGame() {
 
-  function guessFeedback(n) {
-    difference = Math.abs(gameNumber - n);
-    if (difference >= 50) {
-      return "Ice Cold";
-    } else if (difference < 50 && difference >= 30)  {
-      return "Cold";
-    } else if (difference < 30 && difference >= 20) {
-      return "Warm";
-    } else if (difference < 20 && difference >= 10) {
-      return "Warm";
-    } else if (difference < 10 && difference >= 1) {
-      return "Very Hot";
-    } else {
-      return "Winner";
-    }
-  }
-
-  $("#guessButton").keydown(function(e) {
-    if (e.which == 13 && $(this).val().length >= 1) {
-      submitGuess();
-    }
-  })
-  .on("click", function(e) {
-    if ($(this).val().length >=1) {
-      submitGuess();
-      e.preventDefault();
-    }
-  });
-
-  var gameNumber = generateRandomNumber();
-  console.log(gameNumber);
-
-  function submitGuess() {
-    var userGuess = parseInt($("#userGuess").val());
-    if (isNaN(userGuess)) {
-      $("#userGuess").val("");
-      $("#feedback").html("That is not a number");
-    } else {
-      $("#userGuess").val("");
-      $("#feedback").html(guessFeedback(userGuess));
-      addCount = +$("#count").html() + 1;
-      $("#count").html(addCount);
-      $("#guessList").append("<li>" + userGuess + "</li>");
-      if (userGuess == gameNumber) {
-        $("#guessList").html("");
-        $("input").attr("disabled", "disabled");
+    function guessFeedback(n) {
+      difference = Math.abs(winningNumber - n);
+      if (difference >= 50) {
+        return "Ice Cold";
+      } else if (difference < 50 && difference >= 30)  {
+        return "Cold";
+      } else if (difference < 30 && difference >= 20) {
+        return "Warm";
+      } else if (difference < 20 && difference >= 10) {
+        return "Warm";
+      } else if (difference < 10 && difference >= 1) {
+        return "Very Hot";
+      } else {
+        return "Winner";
       }
     }
+
+    $("#guessButton").keydown(function(e) {
+      if (e.which == 13 && $(this).val().length >= 1) {
+        submitGuess();
+      }
+    })
+    .on("click", function(e) {
+      if ($(this).val().length >=1) {
+        submitGuess();
+        e.preventDefault();
+      }
+    });
+
+    function submitGuess() {
+      var userGuess = parseInt($("#userGuess").val());
+      if (isNaN(userGuess)) {
+        $("#userGuess").val("");
+        $("#feedback").html("That is not a number");
+      } else {
+        $("#userGuess").val("");
+        $("#feedback").html(guessFeedback(userGuess));
+        addCount = +$("#count").html() + 1;
+        $("#count").html(addCount);
+        $("#guessList").append("<li>" + userGuess + "</li>");
+        if (userGuess == winningNumber) {
+          $("input").prop("disabled", true);
+        }
+      }
+    }
+
   }
+
+  $("a.new").on("click", function() {
+    $("input").prop("disabled", false);
+    $("#feedback").html("Make your Guess!");
+    $("#count").html("0");
+    $("#guessList").html("");
+    winningNumber = generateRandomNumber();
+  });
+
+  function generateRandomNumber() {
+    var n =  parseInt(Math.random() * 100);
+    console.log(n);
+    return n;
+  }
+
+  var winningNumber = generateRandomNumber();
+  newGame();
 
 });
